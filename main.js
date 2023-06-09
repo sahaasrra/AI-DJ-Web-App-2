de590040e7fa32759fc73caae37a51dececda015
sahaasrra <91600400+sahaasrra@users.noreply.github.com>
2023-06-09 23:44:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+songmastaru = "";
+songmehabooba = "";
+
+function preload()
+{
+    song = loadSound("mastaru.mp3");
+    song = loadSound("Mehabooba.mp3");
+}
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+    image(video,0,0,600,500);
+}
